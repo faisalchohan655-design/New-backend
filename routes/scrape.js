0b7612e85f7c920afc73947a2662dd53844a2e56
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-05 14:23:41 +0500

    Add scraping route to routes/scrape.js

---
+++ routes/scrape.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { startScraping } from '../controllers/scrapeController.js';
+
+const router = express.Router();
+router.post('/scrape', startScraping);
+export default router;
