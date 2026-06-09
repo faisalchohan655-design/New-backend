f8fcce763bfe7620498f7eeec12e3d5ade8fcd13
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-09 15:36:54 +0500

    Delete routes/instagram.js

--- routes/instagram.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { scrapeInstagram } from '../controllers/instagramController.js';
-
-const router = express.Router();
-router.post('/instagram', scrapeInstagram);
-
-export default router;
