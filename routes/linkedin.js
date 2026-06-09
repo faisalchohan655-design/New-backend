b6a243f00ab756b086dd09523a8d2c393cdabfd5
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-09 15:37:23 +0500

    Delete routes/linkedin.js

--- routes/linkedin.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { scrapeLinkedIn } from '../controllers/linkedinController.js';
-
-const router = express.Router();
-router.post('/linkedin', scrapeLinkedIn);
-
-export default router;
