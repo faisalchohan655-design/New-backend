711ec8324addbcb72ce0d70dace6ba334e62f296
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-17 04:33:13 +0500

    Remove save-leads route from social router
    Removed saveSocialLeads route from social.js

--- routes/social.js
+++ routes/social.js
@@ -1,9 +1,7 @@
 import express from 'express';
-import { socialSearch, saveSocialLeads } from '../controllers/socialController.js';
+import { socialSearch } from '../controllers/socialController.js';
 
 const router = express.Router();
-
 router.post('/search', socialSearch);
-router.post('/save-leads', saveSocialLeads);
 
 export default router;
