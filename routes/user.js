d350d778e8d0e773480bbaf17a96303607bc96d7
faisalchohan655-design <faisalchohan655@gmail.com>
2026-06-22 08:00:03 -0700

    Add signup, login, and getMe routes

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getSettings, updateSettings } from '../controllers/userController.js';
+import { getSettings, updateSettings, signup, login, getMe } from '../controllers/userController.js';
 import { auth } from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/me', auth, getMe);
 router.get('/settings', auth, getSettings);
 router.put('/settings', auth, updateSettings);
 
